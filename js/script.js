65fc1e9b475353d9243172b5a82e901b4b5a4e8b
dan <dan@localhost.localdomain>
2019-09-28 19:41:39 +0300

    create a form for the contact us section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,28 @@
 $(document).ready(function(){
     $(".design").click(function(){
         $(".des1").toggle();
-        
+        $(".design").toggle();     
+    });
+    $(".des1").click(function(){
+        $(".design").toggle();
+        $(".des1").toggle();     
     });
     $(".dev").click(function(){
         $(".textdev").toggle();
-    })
+        $(".dev").toggle();
+    });
+    $(".textdev").click(function(){
+        $(".dev").toggle();
+        $(".textdev").toggle();
+    });
+    $(".prod").click(function(){
+        $(".prodText").toggle();
+        $(".prod").toggle();
+    });
+    $(".prodText").click(function(){
+        $(".prod").toggle();
+        $(".prodText").toggle();
+    });
+    
 })
+
